d7f1373ad5c2dfae9423c0b3956b9153fea9fe0a
jsonsivar <jsonsivar@gmail.com>
2020-06-08 17:21:01 -0400

    Add bucket context with creds to get file operation

--- core/textile/client/client_file.go
+++ core/textile/client/client_file.go
@@ -2,12 +2,17 @@ package client
 
 import (
 	"context"
-	"github.com/FleekHQ/space-poc/log"
 	"io"
+
+	"github.com/FleekHQ/space-poc/log"
 )
 
 // GetFile pulls path from bucket writing it to writer if it's a file.
 func (tc *textileClient) GetFile(ctx context.Context, bucketKey string, path string, w io.Writer) error {
+	ctx, _, err := tc.GetBucketContext(defaultPersonalBucketSlug)
+	if err != nil {
+		return err
+	}
 	if err := tc.buckets.PullPath(ctx, bucketKey, path, w); err != nil {
 		log.Error("error in GetFile from textile client", err)
 		return err
@@ -15,4 +20,3 @@ func (tc *textileClient) GetFile(ctx context.Context, bucketKey string, path str
 
 	return nil
 }
-
