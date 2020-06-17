730807a7f5e8bb7dacb4bb6cc87d0a4a5e950f4c
Doug Molina <dmolina79@users.noreply.github.com>
2020-06-17 17:28:15 -0600

    add profile debugging to main.go (#56)

--- cmd/space-poc/main.go
+++ cmd/space-poc/main.go
@@ -3,15 +3,49 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"github.com/FleekHQ/space-poc/app"
 	"github.com/FleekHQ/space-poc/config"
 	"github.com/FleekHQ/space-poc/core/env"
-	"github.com/FleekHQ/space-poc/log"
+	spacelog "github.com/FleekHQ/space-poc/log"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"runtime/pprof"
+
+	_ "net/http/pprof"
+)
+
+var (
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+	memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+	debugMode  = flag.Bool("debug", true, "run daemon with debug mode for profiling")
 )
 
 func main() {
 	// flags
 	flag.Parse()
+
+	// CPU profiling
+	if *debugMode == true {
+		fmt.Println("DEBUG: running daemon with profiler on localhost:6060..")
+		go func() {
+			fmt.Println(http.ListenAndServe("localhost:6060", nil))
+		}()
+	}
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
 
 	// env
 	env := env.New()
@@ -20,9 +54,21 @@ func main() {
 	cfg := config.New(env)
 
 	// setup logger
-	log.New(env)
+	spacelog.New(env)
 	// setup context
 	ctx := context.Background()
 
 	app.Start(ctx, cfg, env)
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
 }
