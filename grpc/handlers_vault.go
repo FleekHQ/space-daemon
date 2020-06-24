2fdb0ad8bca17fc5c5fddbcb5290b74ecbde828c
Daniel Merrill <danielmerrill6@gmail.com>
2020-06-24 17:00:00 -0400

    Add new endpoints to proto (#57)

---
+++ grpc/handlers_vault.go
@@ -0,0 +1,15 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/FleekHQ/space-poc/grpc/pb"
+)
+
+func (srv *grpcServer) BackupKeysByPassphrase(ctx context.Context, request *pb.BackupKeysByPassphraseRequest) (*pb.BackupKeysByPassphraseResponse, error) {
+	return nil, errNotImplemented
+}
+
+func (srv *grpcServer) RecoverKeysByPassphrase(ctx context.Context, request *pb.RecoverKeysByPassphraseRequest) (*pb.RecoverKeysByPassphraseResponse, error) {
+	return nil, errNotImplemented
+}
