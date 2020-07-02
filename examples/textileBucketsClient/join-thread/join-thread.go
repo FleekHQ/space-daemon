7f850f14a2ebf069c60cdea0afc210c16d5ceb79
jsonsivar <jsonsivar@gmail.com>
2020-07-01 23:09:27 -0400

    Working replicator and join thread

--- examples/textileBucketsClient/join-thread/join-thread.go
+++ examples/textileBucketsClient/join-thread/join-thread.go
@@ -3,19 +3,74 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"os/user"
 
 	ma "github.com/multiformats/go-multiaddr"
 	tc "github.com/textileio/go-threads/api/client"
 	"github.com/textileio/go-threads/core/thread"
 	"github.com/textileio/textile/api/common"
 	"github.com/textileio/textile/cmd"
+	"github.com/textileio/textile/core"
 	"google.golang.org/grpc"
 )
 
+var IpfsAddr string
+var MongoUsr string
+var MongoPw string
+var MongoHost string
+
 func main() {
-	addr := os.Args[2]
-	key := os.Args[3]
+	IpfsAddr = os.Getenv("IPFS_ADDR")
+	MongoUsr = os.Getenv("MONGO_USR")
+	MongoPw = os.Getenv("MONGO_PW")
+	MongoHost = os.Getenv("MONGO_HOST")
+
+	addrAPI := cmd.AddrFromStr("/ip4/127.0.0.1/tcp/3006")
+	addrAPIProxy := cmd.AddrFromStr("/ip4/127.0.0.1/tcp/3007")
+	addrThreadsHost := cmd.AddrFromStr("/ip4/0.0.0.0/tcp/4006")
+
+	addrIpfsAPI := cmd.AddrFromStr(IpfsAddr)
+
+	addrGatewayHost := cmd.AddrFromStr("/ip4/127.0.0.1/tcp/8006")
+	addrGatewayURL := "http://127.0.0.1:8006"
+
+	fmt.Println("mongo host: ", MongoHost)
+	addrMongoURI := "mongodb+srv://" + MongoUsr + ":" + MongoPw + "@" + MongoHost
+
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	textile, err := core.NewTextile(ctx, core.Config{
+		RepoPath:        usr.HomeDir + "/.buckd/repo",
+		AddrAPI:         addrAPI,
+		AddrAPIProxy:    addrAPIProxy,
+		AddrThreadsHost: addrThreadsHost,
+		AddrIPFSAPI:     addrIpfsAPI,
+		AddrGatewayHost: addrGatewayHost,
+		AddrGatewayURL:  addrGatewayURL,
+		AddrMongoURI:    addrMongoURI,
+		MongoName:       "buckets",
+		Debug:           false,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer textile.Close()
+	textile.Bootstrap()
+
+	fmt.Println("Welcome to Buckets!")
+	fmt.Println("Your peer ID is " + textile.HostID().String())
+
+	fmt.Println("starting join thread")
+
+	addr := os.Getenv("JOIN_THREAD_ADDR")
+	key := os.Getenv("JOIN_THREAD_KEY")
 
 	m1, _ := ma.NewMultiaddr(addr)
 
@@ -26,7 +81,7 @@ func main() {
 	threadstarget := host
 	opts = append(opts, grpc.WithInsecure())
 	opts = append(opts, grpc.WithPerRPCCredentials(auth))
-	threads, err := tc.NewClient(threadstarget, opts...)
+	threads, err = tc.NewClient(threadstarget, opts...)
 	if err != nil {
 		cmd.Fatal(err)
 	}
@@ -34,12 +89,15 @@ func main() {
 	threadCtx := context.Background()
 	k, err := thread.KeyFromString(key)
 
-	sk := k.Service()
-	rk := k.Read()
+	err = threads.NewDBFromAddr(threadCtx, m1, k)
 
-	threads.NewDBFromAddr(threadCtx, m1, rk)
+	if err != nil {
+		fmt.Println("error new db from addr: ", err)
+	}
 
 	db, err := threads.ListDBs(threadCtx)
+
+	fmt.Println("about to loop thru dbs: ", db)
 
 	for k, v := range db {
 		fmt.Println("looping through thread id: ", k)
--- examples/textileBucketsClient/local-buck/local-buck.go
+++ examples/textileBucketsClient/local-buck/local-buck.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"context"
-	"crypto/rand"
 	"fmt"
 	"os"
 	"os/user"
-	"time"
 
 	"github.com/FleekHQ/space-poc/log"
-	crypto "github.com/libp2p/go-libp2p-crypto"
 	tc "github.com/textileio/go-threads/api/client"
 	"github.com/textileio/go-threads/core/thread"
 	nc "github.com/textileio/go-threads/net/api/client"
 	bc "github.com/textileio/textile/api/buckets/client"
 	"github.com/textileio/textile/api/common"
-	hc "github.com/textileio/textile/api/hub/client"
 	"github.com/textileio/textile/cmd"
 	"github.com/textileio/textile/core"
 	"google.golang.org/grpc"
@@ -36,30 +32,12 @@ func main() {
 	addrAPI := cmd.AddrFromStr("/ip4/127.0.0.1/tcp/3006")
 	addrAPIProxy := cmd.AddrFromStr("/ip4/127.0.0.1/tcp/3007")
 	addrThreadsHost := cmd.AddrFromStr("/ip4/0.0.0.0/tcp/4006")
-	// TODO: replace with local blockstore
-	// TODO: get value from build time
 	addrIpfsAPI := cmd.AddrFromStr(IpfsAddr)
 
 	addrGatewayHost := cmd.AddrFromStr("/ip4/127.0.0.1/tcp/8006")
 	addrGatewayURL := "http://127.0.0.1:8006"
-
-	// PLACEHOLDER: filecoin settings
-
-	// TODO: replace with embedded store
-	// TODO: get value from build time
 	fmt.Println("mongo host: ", MongoHost)
 	addrMongoURI := "mongodb+srv://" + MongoUsr + ":" + MongoPw + "@" + MongoHost
-
-	// TODO: setup logging
-	// if logFile != "" {
-	// 	util.SetupDefaultLoggingConfig(logFile)
-	// }
-
-	// TODO: on shared bucket creation, add hub as replicator
-	// use dbinfo to get keys to give to host, to get hostid
-	// use textile client.GetHostId (against hub we want to
-	// replicate). it will give back a couple just dont use
-	// local one
 
 	usr, err := user.Current()
 	if err != nil {
@@ -76,14 +54,9 @@ func main() {
 		AddrIPFSAPI:     addrIpfsAPI,
 		AddrGatewayHost: addrGatewayHost,
 		AddrGatewayURL:  addrGatewayURL,
-		//AddrPowergateAPI: addrPowergateApi,
-		AddrMongoURI: addrMongoURI,
-		//UseSubdomains:    config.Viper.GetBool("gateway.subdomains"),
-		MongoName: "buckets",
-		//DNSDomain:        dnsDomain,
-		//DNSZoneID:        dnsZoneID,
-		//DNSToken:         dnsToken,
-		Debug: false,
+		AddrMongoURI:    addrMongoURI,
+		MongoName:       "buckets",
+		Debug:           false,
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -121,87 +94,25 @@ func main() {
 	threadCtx := context.Background()
 	threadCtx = common.NewThreadNameContext(threadCtx, "testthreadname")
 	dbID := thread.NewIDV1(thread.Raw, 32)
-	// TODO: store threadid in config
 	if err := threads.NewDB(threadCtx, dbID); err != nil {
 		log.Info("error calling threads.NewDB")
 		log.Fatal(err)
 	}
 
 	ctx = common.NewThreadIDContext(threadCtx, dbID)
-	// create bucket
+
 	buck, err := buckets.Init(ctx, "personal")
 	fmt.Println("info: ", buck)
 
 	db, err := threads.ListDBs(ctx)
 
-	hubHost := getHubHost()
+	fmt.Println("got back from listdbs")
 
 	for k, v := range db {
 		fmt.Println("looping through thread id: ", k)
 		fmt.Println("db info: ", v)
 
-		netc.AddReplicator(ctx, dbID, cmd.AddrFromStr(hubHost))
+		// replicate on hub
+		netc.AddReplicator(ctx, dbID, cmd.AddrFromStr("/ip4/54.188.82.109/tcp/3006"))
 	}
-
-	// replicate on hub
-
-}
-
-func getHubHost() string {
-	var threads *tc.Client
-	var buckets *bc.Client
-	var hub *hc.Client
-	// might need these for other ops so leaving here as commented
-	// out and below
-	// var users *uc.Client
-	var err error
-
-	host := os.Getenv("TXL_HUB_HOST")
-
-	auth := common.Credentials{}
-	var opts []grpc.DialOption
-	hubTarget := host
-	threadstarget := host
-	opts = append(opts, grpc.WithInsecure())
-	opts = append(opts, grpc.WithPerRPCCredentials(auth))
-
-	buckets, err = bc.NewClient(hubTarget, opts...)
-	if err != nil {
-		cmd.Fatal(err)
-	}
-	threads, err = tc.NewClient(threadstarget, opts...)
-	if err != nil {
-		cmd.Fatal(err)
-	}
-	hub, err = hc.NewClient(hubTarget, opts...)
-	if err != nil {
-		cmd.Fatal(err)
-	}
-	// users, err = uc.NewClient(hubTarget, opts...)
-	// if err != nil {
-	// 	cmd.Fatal(err)
-	// }
-
-	key := os.Getenv("TXL_USER_KEY")
-	secret := os.Getenv("TXL_USER_SECRET")
-
-	ctx := context.Background()
-	ctx = common.NewAPIKeyContext(ctx, key)
-	ctx, err = common.CreateAPISigContext(ctx, time.Now().Add(time.Minute*2), secret)
-
-	if err != nil {
-		log.Println("error creating APISigContext")
-		log.Fatal(err)
-	}
-
-	// TODO: get from key manager instead
-	sk, _, err := crypto.GenerateEd25519Key(rand.Reader)
-
-	// TODO: CTX has to be made from session key received from lambda
-	// ctx on next line needs to be rebuilt from the authorization from the lambda
-	tok, err := threads.GetToken(ctx, thread.NewLibp2pIdentity(sk))
-	ctx = thread.NewTokenContext(ctx, tok)
-
-	org, err := hub.GetOrg(ctx)
-	org.Host()
 }
