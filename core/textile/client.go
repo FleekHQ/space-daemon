57ee100a144711dc98d9e6a58e5c4cce223259cc
jsonsivar <jsonsivar@gmail.com>
2020-07-28 17:20:49 -0400

    Temp logging

--- core/textile/client.go
+++ core/textile/client.go
@@ -187,6 +187,7 @@ func (tc *textileClient) Start(ctx context.Context, cfg config.Config) error {
 	// Create default bucket if it doesnt exist
 	defaultBucketExists := false
 	for _, b := range buckets {
+		log.Info("looping through bucket: " + b.Slug())
 		if b.Slug() == defaultPersonalBucketSlug {
 			defaultBucketExists = true
 		}
