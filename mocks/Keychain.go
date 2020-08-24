3490adcca00d3649032a7e8c4ccccc10899f2b51
Daniel Merrill <danielmerrill6@gmail.com>
2020-08-24 18:46:57 -0400

    Fix tests

--- mocks/Keychain.go
+++ mocks/Keychain.go
@@ -181,29 +181,6 @@ func (_m *Keychain) GetStoredPublicKey() (crypto.PubKey, error) {
 	return r0, r1
 }
 
-// GetStoredPublicKey provides a mock function with given fields:
-func (_m *Keychain) GetStoredPublicKey() (crypto.PubKey, error) {
-	ret := _m.Called()
-
-	var r0 crypto.PubKey
-	if rf, ok := ret.Get(0).(func() crypto.PubKey); ok {
-		r0 = rf()
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(crypto.PubKey)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func() error); ok {
-		r1 = rf()
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // ImportExistingKeyPair provides a mock function with given fields: priv
 func (_m *Keychain) ImportExistingKeyPair(priv crypto.PrivKey) error {
 	ret := _m.Called(priv)
