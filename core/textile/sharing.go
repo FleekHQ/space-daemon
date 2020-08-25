1ad2038cfd769ed4fe5c297ed1fb31bda2492b65
jsonsivar <jsonsivar@gmail.com>
2020-08-25 00:31:11 -0400

    Sharing: add todo for integrating buckets acl api

--- core/textile/sharing.go
+++ core/textile/sharing.go
@@ -7,5 +7,6 @@ import (
 )
 
 func (tc *textileClient) ShareFilesViaPublicKey(ctx context.Context, bucketName string, paths []string, pubkeys []crypto.PubKey) error {
+	// TOOD: integrate with new bucket acl api
 	return nil
 }
