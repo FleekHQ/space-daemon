e9eed9b9adb5ab5dcf8890eadeb71ea31e56ba3d
Daniel Merrill <danielmerrill6@gmail.com>
2020-09-02 14:47:18 -0400

    Fix auth hub by removing cached jwt after deleting keys

--- core/textile/textile.go
+++ core/textile/textile.go
@@ -71,7 +71,7 @@ type Client interface {
 	WaitForReady() chan bool
 	Start(ctx context.Context, cfg config.Config) error
 	ShareFilesViaPublicKey(ctx context.Context, bucketName string, paths []string, pubkeys []crypto.PubKey) error
-	RemoveKeys()
+	RemoveKeys() error
 }
 
 type Buckd interface {
