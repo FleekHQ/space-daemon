9918e42ed134082b67baf0ef36c39c705e8a35c0
studna <vojtech.studenka@gmail.com>
2020-09-03 15:00:42 +0200

    Move truncating data into FS service

--- grpc/handlers_account.go
+++ grpc/handlers_account.go
@@ -4,18 +4,9 @@ import (
 	"context"
 	"github.com/FleekHQ/space-daemon/grpc/pb"
 	"github.com/pkg/errors"
-	"os"
-	"os/user"
-	"path/filepath"
 )
 
 func (srv *grpcServer) DeleteAccount(ctx context.Context, request *pb.DeleteAccountRequest) (*pb.DeleteAccountResponse, error) {
-
-	usr, err := user.Current()
-
-	if err != nil {
-		return nil, err
-	}
 
 	if err := srv.fc.Unmount(); err != nil {
 		return nil, errors.Wrap(err, "failed to unmount fuse drive")
@@ -25,18 +16,9 @@ func (srv *grpcServer) DeleteAccount(ctx context.Context, request *pb.DeleteAcco
 		return nil, errors.Wrap(err, "failed to remove keypair")
 	}
 
-	// do we need to shutdown store here?
-
-	// remove data dirs
-	buckdDir := filepath.Join(usr.HomeDir, ".buckd")
-	os.RemoveAll(buckdDir)
-
-	fleekDir := filepath.Join(usr.HomeDir, ".fleek-space")
-	os.RemoveAll(fleekDir)
-
-	// should we also remove .ipfs?
-	// ipfsDir := filepath.Join(usr.HomeDir, ".ipfs")
-	// os.RemoveAll(ipfsDir)
+	if err := srv.sv.TruncateData(ctx); err != nil {
+		return nil, errors.Wrap(err, "error during clean up")
+	}
 
 	return &pb.DeleteAccountResponse{}, nil
 }
