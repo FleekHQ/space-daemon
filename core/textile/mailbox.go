65fb6fa0fe335fddaaec7da760a01018d00d6d41
jsonsivar <jsonsivar@gmail.com>
2020-09-04 12:47:09 -0400

    Hotfix/notifs: need to decrypt msg body before parsing it and some json unmarshalling typos.

--- core/textile/mailbox.go
+++ core/textile/mailbox.go
@@ -34,11 +34,17 @@ type Mailbox interface {
 	ListInboxMessages(ctx context.Context, opts ...client.ListOption) ([]client.Message, error)
 	SendMessage(ctx context.Context, to thread.PubKey, body []byte) (msg client.Message, err error)
 	WatchInbox(ctx context.Context, mevents chan<- mail.MailboxEvent, offline bool) (<-chan cmd.WatchState, error)
+	Identity() thread.Identity
 }
 
-func parseMessage(msg client.Message) (*domain.Notification, error) {
-	var b *domain.MessageBody
-	err := json.Unmarshal([]byte(msg.Body), b)
+func (tc *textileClient) parseMessage(ctx context.Context, msg client.Message) (*domain.Notification, error) {
+	p, err := msg.Open(ctx, tc.mb.Identity())
+	if err != nil {
+		return nil, err
+	}
+
+	b := &domain.MessageBody{}
+	err = json.Unmarshal(p, b)
 
 	if err != nil {
 		return nil, err
@@ -46,7 +52,7 @@ func parseMessage(msg client.Message) (*domain.Notification, error) {
 
 	n := &domain.Notification{
 		ID:               msg.ID,
-		Body:             string(msg.Body),
+		Body:             string(p),
 		NotificationType: (*b).Type,
 		CreatedAt:        msg.CreatedAt.Unix(),
 		ReadAt:           msg.ReadAt.Unix(),
@@ -54,7 +60,7 @@ func parseMessage(msg client.Message) (*domain.Notification, error) {
 
 	switch (*b).Type {
 	case domain.INVITATION:
-		var i *domain.Invitation
+		i := &domain.Invitation{}
 		err := json.Unmarshal((*b).Body, i)
 		if err != nil {
 			return nil, err
@@ -63,7 +69,7 @@ func parseMessage(msg client.Message) (*domain.Notification, error) {
 		n.InvitationValue = *i
 		n.RelatedObject = i
 	case domain.USAGEALERT:
-		var u *domain.UsageAlert
+		u := &domain.UsageAlert{}
 		err := json.Unmarshal((*b).Body, u)
 
 		if err != nil {
@@ -111,7 +117,7 @@ func (tc *textileClient) GetMailAsNotifications(ctx context.Context, seek string
 	}
 
 	for _, n := range notifs {
-		notif, err := parseMessage(n)
+		notif, err := tc.parseMessage(ctx, n)
 		if err != nil {
 			return nil, err
 		}
@@ -144,7 +150,7 @@ func (tc *textileClient) listenForMessages(ctx context.Context) error {
 				// handle new message
 				log.Info("Received mail: " + e.MessageID.String())
 
-				p, err := parseMessage(e.Message)
+				p, err := tc.parseMessage(ctx, e.Message)
 				if err != nil {
 					log.Error("Unable to parse incoming message: ", err)
 				}
