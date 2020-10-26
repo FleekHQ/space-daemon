bc2844561d40740386299e020f431b5dfa5beae8
Perfect Makanju <damiperfect@gmail.com>
2020-10-26 17:55:37 +0100

    Feature: Files Search Schema and stub implementation [ch19245] (#221)
    * Feature: Files Search Schema and stub implementation

--- grpc/handlers.go
+++ grpc/handlers.go
@@ -48,40 +48,7 @@ func (srv *grpcServer) ListDirectories(ctx context.Context, request *pb.ListDire
 		return nil, err
 	}
 
-	dirEntries := make([]*pb.ListDirectoryEntry, 0)
-
-	for _, e := range entries {
-		members := make([]*pb.FileMember, 0)
-
-		for _, m := range e.Members {
-			members = append(members, &pb.FileMember{
-				Address:   m.Address,
-				PublicKey: m.PublicKey,
-			})
-		}
-
-		var backupCount = 0
-		if e.BackedUp {
-			backupCount = 1
-		}
-
-		dirEntry := &pb.ListDirectoryEntry{
-			Path:                e.Path,
-			IsDir:               e.IsDir,
-			Name:                e.Name,
-			SizeInBytes:         e.SizeInBytes,
-			Created:             e.Created,
-			Updated:             e.Updated,
-			FileExtension:       e.FileExtension,
-			IpfsHash:            e.IpfsHash,
-			Members:             members,
-			IsLocallyAvailable:  e.LocallyAvailable,
-			BackupCount:         int64(backupCount),
-			IsBackupInProgress:  e.BackupInProgress,
-			IsRestoreInProgress: e.RestoreInProgress,
-		}
-		dirEntries = append(dirEntries, dirEntry)
-	}
+	dirEntries := mapFileInfoToDirectoryEntry(entries)
 
 	res := &pb.ListDirectoriesResponse{
 		Entries: dirEntries,
@@ -101,6 +68,16 @@ func (srv *grpcServer) ListDirectory(
 		return nil, err
 	}
 
+	dirEntries := mapFileInfoToDirectoryEntry(entries)
+
+	res := &pb.ListDirectoryResponse{
+		Entries: dirEntries,
+	}
+
+	return res, nil
+}
+
+func mapFileInfoToDirectoryEntry(entries []domain.FileInfo) []*pb.ListDirectoryEntry {
 	dirEntries := make([]*pb.ListDirectoryEntry, 0)
 
 	for _, e := range entries {
@@ -135,12 +112,7 @@ func (srv *grpcServer) ListDirectory(
 		}
 		dirEntries = append(dirEntries, dirEntry)
 	}
-
-	res := &pb.ListDirectoryResponse{
-		Entries: dirEntries,
-	}
-
-	return res, nil
+	return dirEntries
 }
 
 func (srv *grpcServer) Subscribe(empty *empty.Empty, stream pb.SpaceApi_SubscribeServer) error {
